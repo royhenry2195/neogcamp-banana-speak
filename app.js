cf771c37778bb27e4384f04ef5edc9be9db17e96
royhenry2195 <109727145+royhenry2195@users.noreply.github.com>
2022-12-21 22:41:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var outputDiv = document.querySelector("#output");
 
 var serverUrl = "https://api.funtranslations.com/translate/yoda.json";
 
-function errorHandler(error){
-    console.log("error occured" , error);
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
 function getTranslateUrl(text){
